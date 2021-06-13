8bff572a25a39852db160023b25f676f33a5ac88
Gent Spahiu <gentsp@moosend.com>
2021-06-13 23:51:26 +0200

    Add new changes and fixes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
-  $("#hero").not(".non-slider").bxSlider();
+  $("#hero").not(".non-slider").bxSlider({
+    touchEnabled: false
+  });
 
   $(".hamburger").click(function(e) {
 		$(this).toggleClass("active");
     $("#main-nav").toggleClass("active");
 		$("body").toggleClass("active-menu");
 	});
+
+  $('.single-gallery').magnificPopup({type:'image', delegate: 'a.image-link', gallery:{enabled:true}});
 });
